7375982bda1889739aa4412a2ecbdeebfc1c4d1c
teru <85771338+teruteru214@users.noreply.github.com>
2024-02-23 08:57:29 +0900

    First Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,18 @@
+import {
+  Inter,
+  Lusitana,
+  Noto_Sans_Javanese,
+  Noto_Serif_JP,
+} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const noto_serif_jp = Noto_Serif_JP({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
